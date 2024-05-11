c3db5215a6fd09b13af2dea8c7ce5b7375658748
AgSilve <86034982+AgSilve@users.noreply.github.com>
2024-05-11 17:24:58 +0530

    feat: Create basic structure of dice roll program - Program prompts user to start or exit.

--- DiceSimulator/Program.cs
+++ DiceSimulator/Program.cs
@@ -1,1 +1,24 @@
-﻿Console.WriteLine("Voila! It's a dice simulator coded in C#");
+﻿// This program gives a visual output when the user chooses to roll a dice.
+Random random = new();
+
+ while (true)
+        {
+            Console.WriteLine("What do you want to do?");
+            Console.WriteLine("1. Roll a dice or 2. Exit");
+            string userInput = Console.ReadLine();
+
+            if (userInput == "1")
+            {
+                Console.WriteLine("You have chosen to roll a dice.");
+                // Code for rolling the dice to be added here
+            }
+            else if (userInput == "2")
+            {
+                Console.WriteLine("Exiting...");
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter 1 to roll a dice or 2 to exit.");
+            }
+        }
