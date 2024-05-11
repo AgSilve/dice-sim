8f4ec58ef6eba9db5057e86738f6c27219d2d2d0
AgSilve <86034982+AgSilve@users.noreply.github.com>
2024-05-11 20:13:09 +0530

    feat: Convert program into System.Main style application.

--- DiceSimulator/Program.cs
+++ DiceSimulator/Program.cs
@@ -1,78 +1,93 @@
 ﻿// This program gives a visual output when the user chooses to roll a dice.
-Random random = new();
+using System;
 
- while (true)
+    namespace DiceSimulator
+    {
+        internal class Program
         {
-            Console.WriteLine("What do you want to do?");
-            Console.WriteLine("1. Roll a dice or 2. Exit");
-            string userInput = Console.ReadLine();
-
-            if (userInput == "1")
+        static void RollDice()
             {
-                Console.WriteLine("You have chosen to roll a dice.");
-                Console.WriteLine("Rolling...");
+                Random random = new();
                 int diceRoll = random.Next(1, 7);
-                switch(diceRoll){
-                case 1:
-                    Console.WriteLine("[-----]");
-                    Console.WriteLine("[     ]");
-                    Console.WriteLine("[  0  ]");
-                    Console.WriteLine("[     ]");
-                    Console.WriteLine("[-----]");
-                    Console.WriteLine("You've rolled 1!");
-                    break;
-                case 2:
-                    Console.WriteLine("[-----]");
-                    Console.WriteLine("[  0  ]");
-                    Console.WriteLine("[     ]");
-                    Console.WriteLine("[  0  ]");
-                    Console.WriteLine("[-----]");
-                    Console.WriteLine("You've rolled 2!");
-                    break;
-                case 3:
-                    Console.WriteLine("[-----]");
-                    Console.WriteLine("[     ]");
-                    Console.WriteLine("[0 0 0]");
-                    Console.WriteLine("[     ]");
-                    Console.WriteLine("[-----]");
-                    Console.WriteLine("You've rolled 3!");
-                    break;
-                case 4:
-                    Console.WriteLine("[-----]");
-                    Console.WriteLine("[0   0]");
-                    Console.WriteLine("[     ]");
-                    Console.WriteLine("[0   0]");
-                    Console.WriteLine("[-----]");
-                    Console.WriteLine("You've rolled 4!");
-                    break;
-                case 5:
-                    Console.WriteLine("[-----]");
-                    Console.WriteLine("[0   0]");
-                    Console.WriteLine("[  0  ]");
-                    Console.WriteLine("[0   0]");
-                    Console.WriteLine("[-----]");
-                    Console.WriteLine("You've rolled 5!");
-                    break;
-                case 6:
-                    Console.WriteLine("[-----]");
-                    Console.WriteLine("[0 0 0]");
-                    Console.WriteLine("[     ]");
-                    Console.WriteLine("[0 0 0]");
-                    Console.WriteLine("[-----]");
-                    Console.WriteLine("You've rolled 6!");
-                    break;
-                default:
-                    Console.WriteLine("Unable to roll a dice.");
-                    break;
+                switch (diceRoll)
+                {
+                    case 1:
+                        Console.WriteLine("[-----]");
+                        Console.WriteLine("[     ]");
+                        Console.WriteLine("[  0  ]");
+                        Console.WriteLine("[     ]");
+                        Console.WriteLine("[-----]");
+                        Console.WriteLine("You've rolled 1!");
+                        break;
+                    case 2:
+                        Console.WriteLine("[-----]");
+                        Console.WriteLine("[  0  ]");
+                        Console.WriteLine("[     ]");
+                        Console.WriteLine("[  0  ]");
+                        Console.WriteLine("[-----]");
+                        Console.WriteLine("You've rolled 2!");
+                        break;
+                    case 3:
+                        Console.WriteLine("[-----]");
+                        Console.WriteLine("[     ]");
+                        Console.WriteLine("[0 0 0]");
+                        Console.WriteLine("[     ]");
+                        Console.WriteLine("[-----]");
+                        Console.WriteLine("You've rolled 3!");
+                        break;
+                    case 4:
+                        Console.WriteLine("[-----]");
+                        Console.WriteLine("[0   0]");
+                        Console.WriteLine("[     ]");
+                        Console.WriteLine("[0   0]");
+                        Console.WriteLine("[-----]");
+                        Console.WriteLine("You've rolled 4!");
+                        break;
+                    case 5:
+                        Console.WriteLine("[-----]");
+                        Console.WriteLine("[0   0]");
+                        Console.WriteLine("[  0  ]");
+                        Console.WriteLine("[0   0]");
+                        Console.WriteLine("[-----]");
+                        Console.WriteLine("You've rolled 5!");
+                        break;
+                    case 6:
+                        Console.WriteLine("[-----]");
+                        Console.WriteLine("[0 0 0]");
+                        Console.WriteLine("[     ]");
+                        Console.WriteLine("[0 0 0]");
+                        Console.WriteLine("[-----]");
+                        Console.WriteLine("You've rolled 6!");
+                        break;
+                    default:
+                        Console.WriteLine("Unable to roll a dice.");
+                        break;
                 }
             }
-            else if (userInput == "2")
+        static void Main()
             {
-                Console.WriteLine("Exiting...");
-                break;
+                while (true)
+                {
+                    Console.WriteLine("What do you want to do?");
+                    Console.WriteLine("1. Roll a dice or 2. Exit");
+                    string userInput = Console.ReadLine();
+
+                    if (userInput == "1")
+                    {
+                        Console.WriteLine("You have chosen to roll a dice.");
+                        Console.WriteLine("Rolling...");
+                        RollDice();
+                    }
+                    else if (userInput == "2")
+                    {
+                        Console.WriteLine("Exiting...");
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid input. Please enter 1 to roll a dice or 2 to exit.");
+                    }
+                }
             }
-            else
-            {
-                Console.WriteLine("Invalid input. Please enter 1 to roll a dice or 2 to exit.");
-            }
-        }
+    }
+}
